1cf1532de95f99f51851b6ced2aae73431effe96
Pedro Souza <pedrosouza.ch453@academico.ifsul.edu.br>
2023-02-16 21:47:41 -0300

    creating challenges repository

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,5 @@
 import { Submission } from "../../domain/entities/submission";
+import { ChallengesRepository } from "../respositories/ChallengesRepository";
 import { StudentsRepository } from "../respositories/StudentsRepository";
 
 type CreateChallengeSubmssionRequest = {
@@ -8,10 +9,18 @@ type CreateChallengeSubmssionRequest = {
 export class CreateChallengeSubmssion {
 
   constructor(
-    private studentsRepository: StudentsRepository
+    private studentsRepository: StudentsRepository,
+    private challengeRepository: ChallengesRepository
   ) {}
 
     async execute({studentId, challengeId}: CreateChallengeSubmssionRequest){
+
+      const student = this.studentsRepository.findById(studentId)
+
+      if(!student){
+        throw new Error('Student does not exist')
+      }
+
       const submssion = Submission.create({
         studentId,
         challengeId
