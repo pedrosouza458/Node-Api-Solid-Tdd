0992a0531a9bb6c055633619eb83c83c606551da
Pedro Souza <pedrosouza.ch453@academico.ifsul.edu.br>
2023-02-16 21:44:43 -0300

    creating students repository

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,11 +1,16 @@
 import { Submission } from "../../domain/entities/submission";
+import { StudentsRepository } from "../respositories/StudentsRepository";
 
 type CreateChallengeSubmssionRequest = {
     studentId: string;
     challengeId: string;
 }
-
 export class CreateChallengeSubmssion {
+
+  constructor(
+    private studentsRepository: StudentsRepository
+  ) {}
+
     async execute({studentId, challengeId}: CreateChallengeSubmssionRequest){
       const submssion = Submission.create({
         studentId,
